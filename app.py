b3bbdfadd1696b6fcd33c4cc050538df1cd78805
Shalyn Lavoie <shalynlavoie@Shalyns-MacBook-Air.local>
2021-06-25 11:45:55 -0400

    updated database variable

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
 from flask import Flask
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI')
+
+db = SQLAlchemy(app)
+
+class Wage(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String)
+
 @app.route('/')
 def index():
     return "Wages"
 
+@app.route('/api/wages/postgres')
+def wage_postgres():
+    wages = db.session.query(Wage)   
+    data = []
+
+    for wage in wages:
+        data.append({
+            "id": wage.id, 
+            "content": wage.content
+        }) 
+
+    return jsonify(data)
+
+
 if __name__ == '__main__':
      app.run(debug = True)
+
+
+
