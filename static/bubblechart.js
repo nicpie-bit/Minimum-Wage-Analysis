c39c3c613cc3f8ec4e7e500d144005b611842968
lamccart15 <lamccart15@gmail.com>
2021-07-10 12:45:21 -0400

    fixing code

--- static/bubblechart.js
+++ static/bubblechart.js
@@ -1,22 +1,21 @@
 var BarChart
 
-
-function BuildBarPlot(data) {
+function BuildBarPlot() {
     var dropdownMenu = d3.select("#selYear");
     var dataset = dropdownMenu.property("value");
     console.log(dataset);
 
     // Fetch data and console log 
-    d3.json(`/api/wages/${dataset}`, function (data) {
+    d3.json(`/api/wages/${dataset}`, function (selectedyear) {
 
-        var yeardata = data;
-        console.log(data)
+        var yeardata = selectedyear;
+        console.log(yeardata)
 
 
         // Define variables
-        var states = data.map(d => d.State)
-        var year = data.map(d => d.Year)
-        var minimumWage = data.map(d => d.EffectiveMinimumWage)
+        var states = yeardata.map(d => d.State)
+        var year = yeardata.map(d => d.Year)
+        var minimumWage = yeardata.map(d => d.EffectiveMinimumWage)
 
 
         // Initiate chart.js    
