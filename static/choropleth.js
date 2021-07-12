066785a48bde2cb0332ecb869471eee8b6eaa138
nmbrouss <nicolebroussard@Nicoles-Air.lan>
2021-07-12 16:18:05 -0400

    fix tooltip position

--- static/choropleth.js
+++ static/choropleth.js
@@ -163,8 +163,8 @@ function createPlot() {
 				console.log(d,i)
 				toolTip.style("visibility", "visible");
 				toolTip.html(`${json.features[i].properties.name}<br>${moneyFormatter(json.features[i].properties.value)}`)
-					.style("left", d3.select(this).attr("path") + "px")
-					.style("top", d3.select(this).attr("path") + "px")
+					.style("left", (d3.event.pageX -800) + "px")
+					.style("top", (d3.event.pageY -2390) + "px")
 			})
 				.on("mouseout", function () {
 					toolTip.style("visibility", "hidden")
