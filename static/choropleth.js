696431bb618c214c1d749b99bdbbd8fb3b7bddaa
nmbrouss <nicolebroussard@Nicoles-Air.lan>
2021-07-12 17:11:49 -0400

    fix tooltip

--- static/choropleth.js
+++ static/choropleth.js
@@ -163,8 +163,8 @@ function createPlot() {
 				console.log(d, i)
 				toolTip.style("visibility", "visible");
 				toolTip.html(`${json.features[i].properties.name}<br>${moneyFormatter(json.features[i].properties.value)}`)
-					.style("left", d3.event.pageX + "px")
-					.style("top", d3.event.pageY+ "px")
+					.style("left", d3.mouse(this)[1] + "px")
+					.style("top", d3.mouse(this)[0] + "px")
 			})
 				.on("mouseout", function () {
 					toolTip.style("visibility", "hidden")
